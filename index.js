c28a855bda50162bc3744ac0c663843544081e82
Ryan Mottola <rmottola68@gmail.com>
2022-06-07 19:31:00 -0400

    commit tree f43280d289a4fd210c5def35282d8bbaf02b81bf

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-// Write your solution in this file!
+let employee = {
+    name: "Sam",
+    streetAddress: "123 Bleeker Street"
+}
+
+let updateEmployeeWithKeyAndValue = (obj, key, val) => {
+    let newObj = {
+        ...obj
+    }
+
+    newObj[key] = val;
+
+    return newObj;
+}
+
+let destructivelyUpdateEmployeeWithKeyAndValue = ( obj, key, val ) => {
+    obj[key] = val;
+    return employee;
+}
+
+let deleteFromEmployeeByKey = (obj, key) => {
+    let newObj = { ...obj };
+    delete newObj[key];
+    return newObj;
+}
+
+let destructivelyDeleteFromEmployeeByKey = (obj, key) => {
+ delete obj[key];
+ return employee;
+}
